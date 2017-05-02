48a88886de0e914f248d198f3d12f8f4cdddab16
Iñaki <inaki.diaz@ikasle.egibide.org>
2017-05-02 22:00:13 +0200

    las bolas se cuelan

---
+++ DIeHardBalls/Assets/GroundControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (-moveVertical, 0.0f,- moveHorizontal);
+		transform.Rotate (movement);
+	}
+}
