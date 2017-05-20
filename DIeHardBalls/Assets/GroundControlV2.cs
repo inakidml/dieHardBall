ff3e08ca1f11cfc66cee2b40790709cfe31d602d
inakidml <inaki.diaz@ikasle.egibide.org>
2017-05-20 22:25:26 +0200

    implementado gyro

--- DIeHardBalls/Assets/GroundControlV2.cs
+++ DIeHardBalls/Assets/GroundControlV2.cs
@@ -3,47 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GroundControlV2 : MonoBehaviour {
+public class GroundControlV2 : MonoBehaviour
+{
 
 	public Text letreroDebug;
 	private Quaternion vectorRotacion;
 
+	private Gyroscope gyro;
+
+	private Quaternion initialRotation;
+	private Quaternion gyroInitialRotation;
+
+
 
 	//https://forum.unity3d.com/threads/sharing-gyroscope-camera-script-ios-tested.241825/
 
-	void Start()
+	void Start ()
 	{
-		Application.targetFrameRate = 60;
+		//Application.targetFrameRate = 60;
+		//para que funcione en el Unity remote
+		if (SystemInfo.supportsGyroscope) {
+			gyro = Input.gyro;
+			gyro.enabled = true;
+		}
+
+		initialRotation = transform.rotation; 
+		gyroInitialRotation = Quaternion.Inverse (Input.gyro.attitude);
+
+	
 
 	}
 
-	void Update()
+	void Update ()
 	{
-		ApplyGyroRotation();
+		ApplyGyroRotation ();
 
 		//Debug
-		letreroDebug.text= "";
+		letreroDebug.text = "";
 		//letreroDebug.text = "x.acel: " + (dir.x).ToString() + "\n";
 		//letreroDebug.text += "z.acel: " + (dir.z).ToString() + "\n";
-		letreroDebug.text += "Rot.x: " + transform.rotation.x.ToString() + "\n";
-		letreroDebug.text += "Rot.z: " + transform.rotation.z.ToString() + "\n";
+		letreroDebug.text += "Rot.x: " + transform.rotation.x.ToString () + "\n";
+		letreroDebug.text += "Rot.z: " + transform.rotation.z.ToString () + "\n";
 
 	}
 
 
 
 
-	void ApplyGyroRotation()
+	void ApplyGyroRotation ()
 	{
-		Quaternion vectorPosicion = Input.gyro.attitude;
-		vectorRotacion.x = vectorPosicion.x;
-		vectorRotacion.z = vectorPosicion.z;
-
-		transform.rotation = Input.gyro.attitude;
-		//transform.Rotate( 0f, 0f, 180f, Space.Self ); // Swap "handedness" of quaternion from gyro.
-		//transform.Rotate( 90f, 180f, 0f, Space.World ); // Rotate to make sense as a camera pointing out the back of your device.
-
+		//http://answers.unity3d.com/questions/289184/gyro-quaternion-offset.html
+		Quaternion offsetRotation = gyroInitialRotation * Input.gyro.attitude;
+	
+		transform.rotation = initialRotation * GyroToUnity (offsetRotation);
 	}
 
+	//https://docs.unity3d.com/ScriptReference/Gyroscope.html
+	// The Gyroscope is right-handed.  Unity is left handed.
+	// Make the necessary change to the camera.
+	Quaternion GyroToUnity (Quaternion q)
+	{
+		
+		//Quaternion iosQuat = new Quaternion (q.x, q.y, -q.z, -q.w);
+		Quaternion iosQuat = new Quaternion (q.x, 0, q.y, -q.w);//Anulo rotación horizontal
+																//según el objeto, 0 y q.y se intercambian 
+		return iosQuat;
+
+	}
 
 }
